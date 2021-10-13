88774e49a9fb2ab655cd41b8dc00bf4e86b5090f
ömer faruk olcay <farukolcayy@gmail.com>
2021-10-13 19:03:07 +0300

    commit(bosch revize mail)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,104 @@
-(function($) {
+(function ($) {
 
-  
-  
-  "use strict";  
 
-  $(window).on('load', function() {
+  const items = document.querySelectorAll(".accordion button");
 
-  /*Page Loader active
-    ========================================================*/
+  function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    for (i = 0; i < items.length; i++) {
+      items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle == 'false') {
+      this.setAttribute('aria-expanded', 'true');
+    }
+  }
+
+  items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+
+  "use strict";
+
+  $(window).on('load', function () {
+
+
+
+
+    /*Page Loader active
+      ========================================================*/
     $('#preloader').fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
+    // Sticky Nav
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 200) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+      } else {
+        $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
     });
 
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2021/05/05 14:00',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> GÜN</div> '
-      +'<div class="time-entry hours"><span>%H</span> SAAT</div> '
-      +'<div class="time-entry minutes"><span>%M</span> DAKİKA</div> '
-      +'<div class="time-entry seconds"><span>%S</span> SANİYE</div> '));
+    jQuery('#clock').countdown('2021/05/05 14:00', function (event) {
+      var $this = jQuery(this).html(event.strftime(''
+        + '<div class="time-entry days"><span>%-D</span> GÜN</div> '
+        + '<div class="time-entry hours"><span>%H</span> SAAT</div> '
+        + '<div class="time-entry minutes"><span>%M</span> DAKİKA</div> '
+        + '<div class="time-entry seconds"><span>%S</span> SANİYE</div> '));
     });
 
     /* slicknav mobile menu active  */
     $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-      });
+      prependTo: '.navbar-header',
+      parentTag: 'liner',
+      allowParentLinks: true,
+      duplicate: true,
+      label: '',
+    });
 
-      /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
+    /* WOW Scroll Spy
+  ========================================================*/
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false
     });
     wow.init();
 
     /* Nivo Lightbox 
     ========================================================*/
     $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
+      effect: 'fadeScale',
+      keyboardNav: true,
+    });
 
     // one page navigation 
     $('.navbar-nav').onePageNav({
-            currentClass: 'active'
-    }); 
+      currentClass: 'active'
+    });
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $('.back-to-top').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
 
-  });      
+  });
 
 }(jQuery));
